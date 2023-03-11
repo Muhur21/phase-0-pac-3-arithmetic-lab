fbed8eb4e0721e19713905c9a7fa1c628c23279d
Mehary Gebremariam <mehary.aynealem3@gmail.com>
2023-03-10 19:48:10 -0600

    submiting arithemetic lab

---
+++ index.js
@@ -0,0 +1,40 @@
+ 
+function add(a, b){
+return a + b
+};
+
+function subtract(a, b){
+return a - b
+};
+
+function multiply(a, b){
+return a*b
+};
+
+function divide(a, b){
+return a/b
+};
+
+let number1 = 5;
+function increment(number1){
+number1++;
+return number1
+}
+
+let number2 = 10;
+function decrement(number2){
+number2--;
+return number2
+};
+
+function makeInt(n) {
+    return parseInt(n, 10)
+ };
+
+ function preserveDecimal(n) {
+    return parseFloat(n)
+ };
+
+
+
+
